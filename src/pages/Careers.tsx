c1d95398d0388300d7d8a266eb5daa44ba58b952
Sashi <47098051+HeyItsSashi@users.noreply.github.com>
2025-07-13 16:46:43 +0530

    Fix JSX parsing error in Careers.tsx

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -150,7 +150,6 @@ const Careers = () => {
             <div className="flex items-center justify-center space-x-3 text-lg">
               <span className="text-2xl"></span>
                 <span className="font-bold group-hover:underline decoration-2 decoration-white underline-offset-2 hover:scale-105 transition-all duration-300">join@848labs.com</span>
-              </span>
             </div>
             <p className="text-gray-300">Tell us what excites you and where you want to make an impact.</p>
           </div>
