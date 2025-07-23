637d142d827c43202b378b4896a3cf06aa1aca1b
Sashi <47098051+HeyItsSashi@users.noreply.github.com>
2025-07-23 09:03:24 +0530

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -78,7 +78,7 @@ Your Next Intelligent Move Starts Here.
 
           </h2>
           <p className="text-xl text-gray-600 max-w-3xl">
-            Partner with 848Labs to move faster, work smarter, and scale impact—powered by purposeful AI and actionable insights, scalable solutions that drive real-world impact.
+            Partner with 848Labs to scale impact powered by purposeful AI and actionable insights, scalable solutions that drive real-world impact.
           </p>
         </div>
 
