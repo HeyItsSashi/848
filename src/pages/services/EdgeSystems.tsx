3cbb88299100ecc5a70fb2aa397083d094644b1e
Sashi <47098051+HeyItsSashi@users.noreply.github.com>
2025-08-26 07:21:51 +0530

    Updated EnterpriseSolutions.tsx

--- src/pages/services/EdgeSystems.tsx
+++ src/pages/services/EdgeSystems.tsx
@@ -328,6 +328,7 @@ const EdgeSystems = () => {
         </div>
       </div>
     </div>
+  )
   );
 };
 
--- src/pages/services/EnterpriseSolutions.tsx
+++ src/pages/services/EnterpriseSolutions.tsx
@@ -367,6 +367,7 @@ const EnterpriseSolutions = () => {
         </div>
       </div>
     </div>
+  )
   );
 };
 
