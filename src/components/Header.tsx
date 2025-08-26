357afbbc5c5615a3b33167d86592276d8a09819b
Sashi <47098051+HeyItsSashi@users.noreply.github.com>
2025-08-26 06:22:01 +0530

    Completely remove all Services links from header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -180,15 +180,6 @@ const Header = () => {
                   </div>
                 </div>
                 
-                <Link
-                  to="/services"
-                  onClick={handleScrollToTop}
-                  className="text-gray-700 hover:text-black transition-all duration-300 relative group hover:scale-105"
-                >
-                  Services
-                  <span className="absolute -bottom-1 left-0 w-0 h-0.5 transition-all duration-300 group-hover:w-full bg-gradient-to-r from-blue-400 to-purple-600"></span>
-                </Link>
-                
               </>
             ) : (
               <Link
